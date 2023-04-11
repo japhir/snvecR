511a1518208732a098b3813b6a79349dc25bc986
Ilja Kocken <i.j.kocken@uu.nl>
2023-04-10 14:57:21 -1000

    add snvec tests for basic call
    it's slow as of yet

--- tests/testthat/test-snvec.R
+++ tests/testthat/test-snvec.R
@@ -6,3 +6,12 @@ test_that("snvecR inputs are checked", {
   expect_error(snvec(tres = -5))
   expect_error(snvec(tres = -Inf))
 })
+
+test_that("snvecR basic call works", {
+  withr::local_options(width = 45)
+  # I test a snapshot of the output
+  # not the messages because they have timestamps
+  expect_snapshot(snvec(quiet = TRUE) |>
+                    dplyr::select(dplyr::all_of(c("age", "sx", "sy", "sx", "epl", "phi", "cp"))) |>
+                    print(n = 100))
+})
--- tests/testthat/test-unwrap.R
+++ tests/testthat/test-unwrap.R
@@ -1,5 +1,7 @@
 test_that("unwrap works", {
   # the first 5 values of lan already have a flip
+  # NOTE: I've also tried expect_identical to test machine precision, this is
+  # NOT the same but only after 12 decimals
   expect_equal(unwrap(ZB18a$lan[1:6])[1:5],
                # output copied from C debugging section
                c(179.999248773572, 180.41472208947, 180.828937891654, 181.245975149062,
