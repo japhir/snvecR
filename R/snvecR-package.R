5c4c31319adc9c1c4c02bac80d79b091f4a8bb06
Ilja Kocken <i.j.kocken@uu.nl>
2024-04-22 23:25:45 -1000

    update how rlang::.data is used

--- R/snvecR-package.R
+++ R/snvecR-package.R
@@ -2,6 +2,8 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom rlang .data
+#' @importFrom rlang .env
 #' @rawNamespace if (getRversion() >= "4.0.0") {importFrom(tools,R_user_dir)} else {importFrom(backports,R_user_dir)}
 ## usethis namespace: end
 NULL
